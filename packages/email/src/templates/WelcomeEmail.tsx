cc8a6214e28355f7de90d383efa668c137bd809b
ronak-pal1 <ronakpaul882@gmail.com>
2025-10-06 11:55:39 +0530

    Added tailwind config for react email

--- packages/email/src/templates/WelcomeEmail.tsx
+++ packages/email/src/templates/WelcomeEmail.tsx
@@ -1,15 +1,24 @@
-import { Html, Head, Body, Container, Text, Button } from "@react-email/components";
-
+import {
+  Html,
+  Head,
+  Body,
+  Container,
+  Text,
+  Button,
+} from "@react-email/components";
+import { Tailwind } from "@react-email/tailwind";
 export function WelcomeEmail({ name }: { name: string }) {
   return (
-    <Html>
-      <Head />
-      <Body style={{ fontFamily: "Arial, sans-serif" }}>
-        <Container>
-          <Text>Hello {name}, welcome to Periskope! 🎉</Text>
-          <Button href="https://periskope.com">Get Started</Button>
-        </Container>
-      </Body>
-    </Html>
+    <Tailwind>
+      <Html>
+        <Head />
+        <Body style={{ fontFamily: "Arial, sans-serif" }}>
+          <Container>
+            <Text>Hello {name}, welcome to Periskope! gaege 🎉</Text>
+            <Button href="https://periskope.com">Get Started</Button>
+          </Container>
+        </Body>
+      </Html>
+    </Tailwind>
   );
 }
